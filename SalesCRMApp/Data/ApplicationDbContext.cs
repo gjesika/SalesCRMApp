8866a7d470e734d1636b55768be82c16e68189c1
gjesika <gjesikaxhafa@gmail.com>
2024-01-27 18:56:22 +0100

    Add project files.

---
+++ SalesCRMApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesCRMApp.Models;
+
+namespace SalesCRMApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Users> users { get; set; }
+
+
+    }
+}
---
+++ SalesCRMApp/Models/Users.cs
@@ -0,0 +1,14 @@
+﻿namespace SalesCRMApp.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public String FirsName { get; set; }
+
+        public String LastName { get; set; }
+
+        public String Username { get; set; }
+
+        public String Source { get; set; }
+    }
+}
